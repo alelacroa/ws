bc42d38eaa494ac0852b2bc5e69919785008c550
alelacroa <alelacroa@gmail.com>
2023-02-24 02:06:02 -0300

    first commit

---
+++ back/index.js
@@ -0,0 +1,82 @@
+const mysql = require("mysql2");
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const express = require("express");
+const app = express();
+
+app.use(cors({ origin: 'http://127.0.0.1:5500' }));
+app.use(bodyParser.json());
+
+
+const connection = mysql.createConnection({
+    host: "aws-sa-east-1.connect.psdb.cloud",
+    user: "o04r9fzfpiirohh14vj1",
+    password: 'pscale_pw_2kr62MzWRRweTJ01a89VLKu6kbjfw56ZEZREB8TuISX',
+    database: "form_data"
+});
+
+
+
+connection.connect(function(error) {
+    if (error) {
+      console.error('Error al conectar: ' + error.stack);
+      return;
+    }
+    console.log('Conectado como ID: ' + connection.threadId);
+  });
+  
+  const axios = require('axios');
+const qs = require('querystring');
+
+app.post('/back/mensajes', (req, res) => {
+  const { nombre, apellido, email, telefono, carrera } = req.body;
+  const sql = `INSERT INTO form_submissions (nombre, apellido, email, telefono, carrera)
+              VALUES ('${nombre}', '${apellido}', '${email}', '${telefono}', '${carrera}')`;
+
+  connection.connect(function(error) {
+    if (error) {
+      return res.status(500).json({ error: 'Error de conexión a la base de datos' });
+    }
+
+    connection.query(sql, function(error, result) {
+      if (error) {
+        return res.status(500).json({ error: 'Error de consulta SQL' });
+      }
+
+      const url = 'http://190.210.65.174/neoapi/webservice.asmx/ExecuteTask08';
+      const params = {
+        idTask: 20,
+        param1: 'NA',
+        param2: req.body.nombre,
+        param3: req.body.apellido,
+        param4: req.body.email,
+        param5: req.body.telefono,
+        param6: req.body.carrera,
+        param7: 'BU.com/postgrado',
+        param8: 'Llámame ahora'
+      };
+      
+      const config = {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      };
+
+      axios.post(url, qs.stringify(params), config)
+        .then(response => {
+          console.log(response.data);
+          return res.json({ success: true });
+        })
+        .catch(error => {
+          console.log(error);
+          return res.status(500).json({ error: 'Error en la llamada a la API' });
+        });
+    });
+  });
+});
+
+  
+  const port = 8000;
+  app.listen(8000, function() {
+    console.log('Server running');
+  });
---
+++ front/script.js
@@ -0,0 +1,31 @@
+const form = document.getElementById("formulario");
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const data = {
+    nombre: formData.get("nombre"),
+    apellido: formData.get("apellido"),
+    email: formData.get("email"),
+    telefono: formData.get("telefono"),
+    carrera: formData.get("carrera"),
+  };
+
+  const response = await fetch("http://localhost:8000/back/mensajes", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+    
+  });
+  console.log(data)
+  
+if (response.ok) {
+    window.location.href = "agradecimiento.html";
+    
+  } else {
+    alert("Error al enviar el formulario. Por favor, inténtalo de nuevo más tarde.");
+  }
+});
